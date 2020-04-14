dc9b93a7bc0af2be56080d4f0fb0ab5b00e975d5
Miguel de Alba <miguelalba89@gmail.com>
2020-04-14 13:13:38 +0200

    Add H2 database

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,20 +1,23 @@
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpServer;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 class Application {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
 
-        Source[] sources = new Source[]{
-                new Source(0, "Events (meeting or conference)", ""),
-                new Source(17, "Expert knowledge elicitation", ""),
-                new Source(10, "Human health surveillance data", "laboratory diagnostic data")};
+        // Starts DB
+        final Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+        initDB(connection);
 
         int serverPort = 8000;
 
@@ -23,9 +26,9 @@ class Application {
         final ObjectMapper mapper = new ObjectMapper();
 
         // /api/sources?id=:id
-        server.createContext("/api/sources", (exchange -> {
+        server.createContext("/api/sources", exchange -> {
 
-            final String responseText;
+            String responseText = "";
 
             String query = exchange.getRequestURI().getQuery();
             if (query != null) {
@@ -35,14 +38,29 @@ class Application {
                 // If an id is passed then only return the Source with that id. If other parameter is passed, ignore it
                 // and return all the sources.
                 if (params.containsKey("id")) {
-                    int id = Integer.parseInt(params.get("id"));
-                    responseText = mapper.writeValueAsString(sources[id]);
+                    try {
+                        int id = Integer.parseInt(params.get("id"));
+                        Source source = getSource(connection, id);
+                        responseText = mapper.writeValueAsString(source);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
                 } else {
-                    responseText = mapper.writeValueAsString(sources);
+                    try {
+                        Source[] sources = getSources(connection);
+                        responseText = mapper.writeValueAsString(sources);
+                    } catch (SQLException exception) {
+                        exception.printStackTrace(); // TODO: ...
+                    }
                 }
             } else {
                 // Return all the sources
-                responseText = mapper.writeValueAsString(sources);
+                try {
+                    Source[] sources = getSources(connection);
+                    responseText = mapper.writeValueAsString(sources);
+                } catch (SQLException exception) {
+                    exception.printStackTrace(); // TODO: ...
+                }
             }
 
             exchange.sendResponseHeaders(200, responseText.getBytes().length);
@@ -52,7 +70,7 @@ class Application {
             }
 
             exchange.close();
-        }));
+        });
 
         server.setExecutor(null); // Creates a default executor
         server.start();
@@ -69,5 +87,61 @@ class Application {
         }
 
         return params;
+    }
+
+    private static void initDB(Connection connection) throws SQLException, IOException {
+        Map<String, String> tableFiles = new HashMap<>();
+        tableFiles.put("sources", "sources.sql");
+        tableFiles.put("rights", "rights.sql");
+
+        for (Map.Entry<String, String> entry : tableFiles.entrySet()) {
+            String sourceFile = Application.class.getClassLoader().getResource(entry.getValue()).getFile();
+            String sql = new String(Files.readAllBytes(Paths.get(sourceFile)));
+
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+
+            String tableName = entry.getKey();
+            int numberOfRows = getRowCount(connection, tableName);
+            System.out.printf("Table %s created with %d rows\n", tableName, numberOfRows);
+        }
+    }
+
+    private static int getRowCount(Connection connection, String table) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT COUNT(1) FROM " + table);
+        resultSet.next();
+
+        return resultSet.getInt(1);
+    }
+
+    private static Source[] getSources(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * from sources");
+
+        ArrayList<Source> sourceList = new ArrayList<>();
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            String comment = resultSet.getString("comment");
+
+            sourceList.add(new Source(id, name, comment));
+        }
+
+        return sourceList.toArray(new Source[0]);
+    }
+
+    private static Source getSource(Connection connection, int id) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM sources WHERE id = " + id);
+
+        if (resultSet.next()) {
+            String name = resultSet.getString("name");
+            String comment = resultSet.getString("comment");
+
+            return new Source(id, name, comment);
+        } else {
+            return null;
+        }
     }
 }
---
+++ src/main/resources/rights.sql
@@ -0,0 +1,92 @@
+CREATE TABLE rights(
+    id VARCHAR(50) not NULL,
+    name VARCHAR(255) not NULL,
+    url VARCHAR(255),
+    PRIMARY KEY (id));
+
+INSERT INTO rights (id, name, url) VALUES ('AFL-3.0', 'Academic Free License 3.0', 'http://www.opensource.org/licenses/AFL-3.0');
+INSERT INTO rights (id, name, url) VALUES ('APL-1.0', 'Adaptive Public License 1.0', 'http://www.opensource.org/licenses/APL-1.0');
+INSERT INTO rights (id, name, url) VALUES ('Apache-1.1', 'Apache Software License 1.1', 'http://www.opensource.org/licenses/Apache-1.1');
+INSERT INTO rights (id, name, url) VALUES ('Apache-2.0', 'Apache Software License 2.0', 'http://www.opensource.org/licenses/Apache-2.0');
+INSERT INTO rights (id, name, url) VALUES ('APSL-2.0', 'Apple Public Source License 2.0', 'http://www.opensource.org/licenses/APSL-2.0');
+INSERT INTO rights (id, name, url) VALUES ('Artistic-2.0', 'Artistic License 2.0', 'http://www.opensource.org/licenses/Artistic-2.0');
+
+-- Attribution Assurance Licenses	AAL	http://www.opensource.org/licenses/AAL
+-- BSD 3-Clause "New" or "Revised" License (BSD-3-Clause)	BSD-3-Clause	http://www.opensource.org/licenses/BSD-3-Clause
+-- Boost Software License 1.0	BSL-1.0	http://www.opensource.org/licenses/BSL-1.0
+-- CC0 1.0	CC0-1.0	https://creativecommons.org/publicdomain/zero/1.0/
+-- CUA Office Public License 1.0	CUA-OPL-1.0	http://www.opensource.org/licenses/CUA-OPL-1.0
+-- CeCILL License 2.1	CECILL-2.1	http://www.opensource.org/licenses/CECILL-2.1
+-- Common Development and Distribution License 1.0	CDDL-1.0	http://www.opensource.org/licenses/CDDL-1.0
+-- Common Public Attribution License 1.0	CPAL-1.0	http://www.opensource.org/licenses/CPAL-1.0
+-- Computer Associates Trusted Open Source License 1.1 (CATOSL-1.1)	CATOSL-1.1	http://www.opensource.org/licenses/CATOSL-1.1
+-- Creative Commons Attribution 4.0	CC-BY-4.0	https://creativecommons.org/licenses/by/4.0/
+-- Creative Commons Attribution Share-Alike 4.0	CC-BY-SA-4.0	https://creativecommons.org/licenses/by-sa/4.0/
+-- Creative Commons Attribution-NoDerivs 4.0	CC BY-ND 4.0	https://creativecommons.org/licenses/by-nd/4.0/
+-- Creative Commons Attribution-NonCommercial 4.0	CC-BY-NC-4.0	https://creativecommons.org/licenses/by-nc/4.0/
+-- Creative Commons Attribution-NonCommercial-NoDerivatives 4.0	CC-BY-NC-ND-4.0	https://creativecommons.org/licenses/by-nc-nd/4.0/
+-- Creative Commons Attribution-NonCommercial-ShareAlike 4.0	CC BY-NC-SA 4.0	https://creativecommons.org/licenses/by-nc-sa/4.0/
+-- EU DataGrid Software License	EUDatagrid	http://www.opensource.org/licenses/EUDatagrid
+-- Eclipse Public License 1.0	EPL-1.0	http://www.opensource.org/licenses/EPL-1.0
+-- Educational Community License 2.0	ECL-2.0	http://www.opensource.org/licenses/ECL-2.0
+-- Eiffel Forum License 2.0	EFL-2.0	http://www.opensource.org/licenses/EFL-2.0
+-- Entessa Public License	Entessa	http://www.opensource.org/licenses/Entessa
+-- European Union Public License 1.1	EUPL-1.1	http://www.opensource.org/licenses/EUPL-1.1
+-- Fair License	Fair	http://www.opensource.org/licenses/Fair
+-- Frameworx License 1.0	Frameworx-1.0	http://www.opensource.org/licenses/Frameworx-1.0
+-- GNU Affero General Public License v3	AGPL-3.0	http://www.opensource.org/licenses/AGPL-3.0
+-- GNU Free Documentation License 1.3 with no cover texts and no invariant sections	GFDL-1.3-no-cover-texts-no-invariant-sections	http://www.opendefinition.org/licenses/gfdl
+-- GNU General Public License 2.0	GPL-2.0	http://www.opensource.org/licenses/GPL-2.0
+-- GNU General Public License 3.0	GPL-3.0	http://www.opensource.org/licenses/GPL-3.0
+-- GNU Lesser General Public License 2.1	LGPL-2.1	http://www.opensource.org/licenses/LGPL-2.1
+-- GNU Lesser General Public License 3.0	LGPL-3.0	http://www.opensource.org/licenses/LGPL-3.0
+-- IBM Public License 1.0	IPL-1.0	http://www.opensource.org/licenses/IPL-1.0
+-- IPA Font License	IPA	http://www.opensource.org/licenses/IPA
+-- ISC License	ISC	http://www.opensource.org/licenses/ISC
+-- LaTeX Project Public License 1.3c	LPPL-1.3c	http://www.opensource.org/licenses/LPPL-1.3c
+-- License Not Specified	notspecified	
+-- Lucent Public License ("Plan9") 1.0	LPL-1.0	http://www.opensource.org/licenses/LPL-1.0
+-- MIT License	MIT	http://www.opensource.org/licenses/MIT
+-- Microsoft Public License	MS-PL	http://www.opensource.org/licenses/MS-PL
+-- Microsoft Reciprocal License	MS-RL	http://www.opensource.org/licenses/MS-RL
+-- MirOS Licence	MirOS	http://www.opensource.org/licenses/MirOS
+-- Motosoto License	Motosoto	http://www.opensource.org/licenses/Motosoto
+-- Mozilla Public License 1.1	MPL-1.1	http://www.opensource.org/licenses/MPL-1.1
+-- Multics License	Multics	http://www.opensource.org/licenses/Multics
+-- NASA Open Source Agreement 1.3	NASA-1.3	http://www.opensource.org/licenses/NASA-1.3
+-- NTP License	NTP	http://www.opensource.org/licenses/NTP
+-- Naumen Public License	Naumen	http://www.opensource.org/licenses/Naumen
+-- Nethack General Public License	NGPL	http://www.opensource.org/licenses/NGPL
+-- Nokia Open Source License	Nokia	http://www.opensource.org/licenses/Nokia
+-- Non-Profit Open Software License 3.0	NPOSL-3.0	http://www.opensource.org/licenses/NPOSL-3.0
+-- OCLC Research Public License 2.0	OCLC-2.0	http://www.opensource.org/licenses/OCLC-2.0
+-- Open Data Commons Attribution License 1.0	ODC-BY-1.0	http://www.opendefinition.org/licenses/odc-by
+-- Open Data Commons Open Database License 1.0	ODbL-1.0	http://www.opendefinition.org/licenses/odc-odbl
+-- Open Data Commons Public Domain Dedication and Licence 1.0	PDDL-1.0	http://www.opendefinition.org/licenses/odc-pddl
+-- Open Font License 1.1	OFL-1.1	http://www.opensource.org/licenses/OFL-1.1
+-- Open Government Licence 2.0 (United Kingdom)	OGL-UK-2.0	https://www.nationalarchives.gov.uk/doc/open-government-licence/version/2/
+-- Open Group Test Suite License	OGTSL	http://www.opensource.org/licenses/OGTSL
+-- Open Software License 3.0	OSL-3.0	http://www.opensource.org/licenses/OSL-3.0
+-- Other (Attribution)	other-at	
+-- Other (Non-Commercial)	other-nc	
+-- Other (Not Open)	other-closed	
+-- Other (Open)	other-open	
+-- Other (Public Domain)	other-pd	
+-- PHP License 3.0	PHP-3.0	http://www.opensource.org/licenses/PHP-3.0
+-- PostgreSQL License	PostgreSQL	http://www.opensource.org/licenses/PostgreSQL
+-- Python License 2.0	Python-2.0	http://www.opensource.org/licenses/Python-2.0
+-- Q Public License 1.0	QPL-1.0	http://www.opensource.org/licenses/QPL-1.0
+-- RealNetworks Public Source License 1.0	RPSL-1.0	http://www.opensource.org/licenses/RPSL-1.0
+-- Reciprocal Public License 1.5	RPL-1.5	http://www.opensource.org/licenses/RPL-1.5
+-- Ricoh Source Code Public License	RSCPL	http://www.opensource.org/licenses/RSCPL
+-- Simple Public License 2.0	SimPL-2.0	http://www.opensource.org/licenses/SimPL-2.0
+-- Sleepycat License	Sleepycat	http://www.opensource.org/licenses/Sleepycat
+-- Sun Public License 1.0	SPL-1.0	http://www.opensource.org/licenses/SPL-1.0
+-- Sybase Open Watcom Public License 1.0	Watcom-1.0	http://www.opensource.org/licenses/Watcom-1.0
+-- Talis Community License	Talis	http://www.opendefinition.org/licenses/tcl
+-- University of Illinois/NCSA Open Source License	NCSA	http://www.opensource.org/licenses/NCSA
+-- Vovida Software License 1.0	VSL-1.0	http://www.opensource.org/licenses/VSL-1.0
+-- W3C License	W3C	http://www.opensource.org/licenses/W3C
+-- X.Net License	Xnet	http://www.opensource.org/licenses/Xnet
+-- Zope Public License 2.0	ZPL-2.0	http://www.opensource.org/licenses/ZPL-2.0
+-- zlib/libpng license	Zlib	http://www.opensource.org/licenses/Zlib
