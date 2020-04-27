b16303ababd338188acd5644899fa5d7228d03ce
Miguel de Alba <miguelalba89@gmail.com>
2020-04-27 18:36:56 +0200

    Merge unit category into unit

--- src/main/resources/data/initialdata/unit.sql
+++ src/main/resources/data/initialdata/unit.sql
@@ -1,3 +1,8 @@
+CREATE TABLE unit_category (
+    id INTEGER not NULL,
+    name VARCHAR(255) not NULL,
+    PRIMARY KEY(id));
+
 CREATE TABLE unit (
     id INTEGER NOT NULL,
     name VARCHAR(64) NOT NULL,
@@ -6,6 +11,32 @@ CREATE TABLE unit (
     category_id INTEGER,
     PRIMARY KEY(id),
     FOREIGN KEY(category_id) REFERENCES unit_category(id));
+
+INSERT INTO unit_category (id, name) VALUES (1, 'Number Content (count/mass)');
+INSERT INTO unit_category (id, name) VALUES (2, 'Number Concentration (count/vol)');
+INSERT INTO unit_category (id, name) VALUES (3, 'Number Aeric (number/area)');
+INSERT INTO unit_category (id, name) VALUES (4, 'Mass Ratio');
+INSERT INTO unit_category (id, name) VALUES (5, 'Mass Concentration');
+INSERT INTO unit_category (id, name) VALUES (6, 'Arbitrary Fraction');
+INSERT INTO unit_category (id, name) VALUES (7, 'Energy Content');
+INSERT INTO unit_category (id, name) VALUES (8, 'Energy Content Rate');
+INSERT INTO unit_category (id, name) VALUES (9, 'Unknown');
+INSERT INTO unit_category (id, name) VALUES (10, 'Pressure');
+INSERT INTO unit_category (id, name) VALUES (11, 'Mole Content');
+INSERT INTO unit_category (id, name) VALUES (12, 'Length');
+INSERT INTO unit_category (id, name) VALUES (13, 'Temperature');
+INSERT INTO unit_category (id, name) VALUES (14, 'Number Difference');
+INSERT INTO unit_category (id, name) VALUES (15, 'Number Content Difference');
+INSERT INTO unit_category (id, name) VALUES (16, 'Number Fraction');
+INSERT INTO unit_category (id, name) VALUES (17, 'Number Concentration Difference');
+INSERT INTO unit_category (id, name) VALUES (18, 'Mass');
+INSERT INTO unit_category (id, name) VALUES (19, 'Number');
+INSERT INTO unit_category (id, name) VALUES (20, 'Time');
+INSERT INTO unit_category (id, name) VALUES (21, 'Volume Ratio');
+INSERT INTO unit_category (id, name) VALUES (22, 'Dimensionless Quantity');
+INSERT INTO unit_category (id, name) VALUES (23, 'True/False Value');
+INSERT INTO unit_category (id, name) VALUES (24, 'Volume Concentration');
+INSERT INTO unit_category (id, name) VALUES (25, 'Other');
 
 INSERT INTO unit (id, name, comment, category_id) VALUES (0, '[]', 'no name', 22);
 INSERT INTO unit (id, name, comment, category_id) VALUES (1, '[aw]', 'Water activity', 22);
